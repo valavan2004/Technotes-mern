734f7a8ffbaa2efeb332fc80dc7684eae0ca1a14
valavan2004 <valavan.k2022ece@sece.ac.in>
2024-08-07 21:13:53 +0530

    task-1

---
+++ UserStories.md
@@ -0,0 +1,22 @@
+# User Stories for TechNotes
+
+1. [ ] replace current sticky note system
+2. [ ] add a public facing page with basic contact info
+3. [ ] add an employee login to the notes app
+4. [ ] provide a welcome page after login
+5. [ ] provide easy navigation
+6. [ ] display current user and assigned role
+7. [ ] provide a logout option
+8. [ ] require users to login at least once per weeks
+9. [ ] provide a way to remove employee acess asap if needed
+10. [ ] notes are assigned to specific employees
+11. [ ] notes have a ticket #, title,note body, created & updated dates
+12. [ ] notes are either OPEN or COMPLETED
+13. [ ] Users can be employees, managers, or admins
+14. [ ] notes can only be deleted by managers or admins
+15. [ ] anyone can create a note (when customer checks-in)
+16. [ ] employees can only view, edit, and delete all notes
+17. [ ] managers and admins van view, edit, and delete all notes
+18. [ ] only managers and admins can acess User settings
+19. [ ] only managers and admins can create new User 
+20.[ ] desktop mode is most important but should be available in mobile
---
+++ server.js
@@ -0,0 +1,42 @@
+//  const express = require("express")
+//  const  app = express
+//  const  path = require("path")
+//  const PORT = process.env.PORT || 3000
+
+// app.use("/", express.static(path.join(__dirname,"/public")))
+// app.use("/", require("./routes/root"))
+
+// app.all,("*", (req,res)  => {
+//     res.status(404)
+//     if(req.accepts(html)) {
+//         res.sendFile(path.join(__dirname,'views',"404.html"))
+//     }
+//     elseif(req.accepts('json')) {
+//         res.json({error: "Not found"})
+//     }
+//     else{
+//         res.type("txt").send("Not found")
+//     }
+// })
+//  app.listen (PORT, () => console.log(`server is running on port ${PORT}`))
+
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = process.env.PORT || 3500;
+
+app.use("/", express.static(path.join(__dirname, "/public")));
+app.use("/", require("./routes/root"));
+
+app.all("*", (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', "404.html"));
+    } else if (req.accepts('json')) {
+        res.json({ message: "Not found" });
+    } else {
+        res.type("txt").send("404 Not found");
+    }
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
